108b561d707d24f6d968673f714b47013dd340cc
Guilherme Rodrigues <girse@iscte-iul.pt>
2020-06-15 02:33:58 +0100

    Update AppTest.java

--- esii/src/test/java/grupo22/esii/AppTest.java
+++ esii/src/test/java/grupo22/esii/AppTest.java
@@ -46,7 +46,7 @@ public class AppTest {
 		String html = app.getHTMl();
 
 		String test = "<!DOCTYPE html><html><head><style>table {font-family: arial, sans-serif;border-collapse: collapse;width: 100%;}td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}tr:nth-child(even) {background-color: #dddddd;}</style>"
-				+ "</head><body><h2>HTML Table</h2><table><tr><th>File timestamp</th><th>File name</th><th>File tag</th><th> Tag Description</th><th> Spread Visualization Link</th></tr>";
+				+ "</head><body><h2>Covid Spread Table</h2><table><tr><th>File timestamp</th><th>File name</th><th>File tag</th><th> Tag Description</th><th> Spread Visualization Link</th></tr>";
 		test+="Test";
 		test+="</table></body></html>";
 		assertEquals(test, app.getHTMl());
